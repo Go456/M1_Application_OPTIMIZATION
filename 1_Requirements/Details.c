bb9de360393b9b9523e2154ea72d600884c85204
Go456 <srirangamgowtham@gmail.com>
2021-11-22 11:32:06 +0530

    commit

---
+++ 1_Requirements/Details.c
@@ -0,0 +1,13 @@
+/**Introduction.
+*  This project is on reminder to a person according to the schedule,as we know we have a google reminder for the links or any mails that are recent by authority
+*that is add to our calendar but this project is something like we add some beep sound or alarm to awake the user to attend the session at a particular time .
+*Research 
+        *  About google reminder
+        *  https://www.mksguide.com/google-reminders-vs-tasks/
+
+*Defining Our System :
+                   
+          **//  In our system we are adding the alarm for reminder.
+
+
+            
