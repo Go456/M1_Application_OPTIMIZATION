6362ac3edc8dd197a0aca091c53ba739d7babf53
Go456 <57907539+Go456@users.noreply.github.com>
2021-11-25 10:50:17 +0530

    Update test.h

--- 3_Implementation/Server/test.h
+++ 3_Implementation/Server/test.h
@@ -1,1 +1,33 @@
+#include "unity.h"
+#include <config.h>
+#include<function.h>
+#include<sock.h>
 
+#define PROJECT_NAME    "Reminder"
+
+
+
+
+void test_login(void);
+void test_errorMessage(void);
+
+
+
+void setUp(){}
+
+void tearDown(){}
+
+
+int main()
+{
+
+  UNITY_BEGIN();
+
+
+  RUN_TEST(test_login);<br/>
+  RUN_TEST(test_menuExit);
+  RUN_TEST(test_errorMessage);
+
+  
+  return UNITY_END();
+}
